7f5f1be7fd358bfe36b91c1c36be9ef482575e7d
Alexander Goussas <goussasalexander@gmail.com>
2025-06-01 19:31:03 -0500

    feat: add home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/button'
+import AppNavigationMenu from '@/components/navbar'
+import { Input } from '@/components/ui/input'
+import { toast } from "sonner"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('')
+
+  const checkUsername = () => {
+    if (!username) {
+      toast.warning('Please enter the username')
+    }
+  }
+
+  const onCreateRoom = () => {
+    checkUsername()
+  }
+
+  const onJoinRoom = () => {
+    checkUsername()
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen">
+      <AppNavigationMenu />
+      <div className="flex flex-col items-center justify-center h-100">
+        <div className="flex flex-col gap-4">
+          <Input placeholder="Your username..." value={username} onInput={e => setUsername(e.target.value)} />
+          <Button className="text-xl" onClick={onCreateRoom}>Create Room</Button>
+          <Button className="text-xl" onClick={onJoinRoom}>Join Room</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+export default function AppNavigationMenu() {
+  return (
+    <nav className="flex p-4">
+      <div className="text-2xl font-bold">
+        CHARADES APP
+      </div>
+    </nav>
+  )
+}
+
